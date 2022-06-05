894db9bcf72e0e3156ccd1faf59c1355f1d92139
yixin <927871050@qq.com>
2022-06-05 21:50:10 +0800

    把provider（提供者）注册到Eureka 使用actuator配置描述信息 使用discovery获取一些信息

--- springcloud-provider-dept-8001/src/main/java/com/yixin/springcloud/controller/DeptController.java
+++ springcloud-provider-dept-8001/src/main/java/com/yixin/springcloud/controller/DeptController.java
@@ -3,6 +3,8 @@ package com.yixin.springcloud.controller;
 import com.yixin.springcloud.pojo.Dept;
 import com.yixin.springcloud.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +17,9 @@ public class DeptController {
 
     @Autowired
     private DeptService deptService;
+
+    @Autowired
+    private DiscoveryClient client;
 
     @PostMapping("/dept/add")
     public boolean addDept(Dept dept){
@@ -31,5 +36,24 @@ public class DeptController {
         return deptService.queryAll();
     }
 
+//团队协作的时候需要用到的
+    @GetMapping("/dept/discovery")
+    public Object discovery(){
+        //获取微服务列表的清单
+        List<String> services = client.getServices();
+        System.out.println("discovery==>service"+services);
 
+        List<ServiceInstance> instances = client.getInstances("SPRINGCLOUD-PROVIDER-DEPT");
+
+        for (ServiceInstance instance: instances){
+            System.out.println(
+                    instance.getHost()+
+                    instance.getPort()+
+                    instance.getUri()+
+                    instance.getServiceId()
+
+            );
+        }
+        return client;
+    }
 }
